0887047c88d2626c3a80f3a56510a2cc8530ff11
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2021-06-12 20:25:23 +0700

    :hammer: Responsive menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,24 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       fixed
       app
@@ -7,25 +26,26 @@
     >
       <v-toolbar-title>
         <v-img
-          max-height="60"
+          max-height="50"
           max-width="110"
           contain
           :src="require('@/assets/img/logo.png')"
         ></v-img>
       </v-toolbar-title>
       <v-spacer />
-      <v-btn text color="primary">
+      <v-btn text color="primary" class="hidden-sm-and-down">
         Beranda
       </v-btn>
-      <v-btn text color="accent">
+      <v-btn text color="accent" class="hidden-sm-and-down">
         Tentang
       </v-btn>
-      <v-btn text color="accent">
+      <v-btn text color="accent" class="hidden-sm-and-down">
         Kontak
       </v-btn>
-      <v-btn text color="accent">
+      <v-btn text color="accent" class="hidden-sm-and-down">
         Blog
       </v-btn>
+      <v-app-bar-nav-icon class="hidden-md-and-up" color="primary" @click.stop="drawer = !drawer" />
     </v-app-bar>
     <v-main>
       <v-container>
@@ -50,7 +70,27 @@
 export default {
   data () {
     return {
-      title: 'RedRoofed'
+      title: 'RedRoofed',
+      drawer: false,
+      clipped: false,      
+      items: [
+        {
+          title: 'Beranda',
+          to: '/'
+        },
+        {
+          title: 'Tentang',
+          to: '/tentang'
+        },
+        {
+          title: 'Kontak',
+          to: '/kontak'
+        },
+        {
+          title: 'Blog',
+          to: '/blog'
+        }
+      ]
     }
   }
 }
