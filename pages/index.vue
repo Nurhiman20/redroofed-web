1681fd73a6a4fb732d0cec22d75e5073022d6d46
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2021-06-13 10:03:26 +0700

    :sparkles: section(About us): Slicing

--- pages/index.vue
+++ pages/index.vue
@@ -22,14 +22,14 @@
 
     <div class="py-12">
       <v-row justify="center" align="center">
-        <v-col cols="12" sm="6" md="6" lg="6" xl="6" class="mb-n4">
+        <v-col cols="12" md="6" lg="6" xl="6" class="mb-n4">
           <div>
-            <h1>Penawaran Kami</h1>
+            <h1 class="font-weight-medium">Penawaran Kami</h1>
             <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, laboriosam accusantium dolores culpa reiciendis.</p>
           </div>
         </v-col>
-        <v-col cols="12" sm="6" md="6" lg="6" xl="6" class="mb-n4"></v-col>
-        <v-col cols="12" sm="4" md="4" lg="4" xl="4">
+        <v-col cols="12" md="6" lg="6" xl="6" class="mb-n4"></v-col>
+        <v-col cols="12" md="4" lg="4" xl="4">
           <v-card min-height="250" class="d-flex flex-column justify-center">
             <div class="pa-4">
               <v-img
@@ -44,7 +44,7 @@
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col cols="12" sm="4" md="4" lg="4" xl="4">
+        <v-col cols="12" md="4" lg="4" xl="4">
           <v-card min-height="250" class="d-flex flex-column justify-center">
             <div class="pa-4">
               <v-img
@@ -59,7 +59,7 @@
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col cols="12" sm="4" md="4" lg="4" xl="4">
+        <v-col cols="12" md="4" lg="4" xl="4">
           <v-card min-height="250" class="d-flex flex-column justify-center">
             <div class="pa-4">
               <v-img
@@ -75,6 +75,42 @@
           </v-card>
         </v-col>
       </v-row>  
+    </div>
+
+    <div class="py-12">
+      <v-row justify="center" align="center">
+        <v-col cols="12" md="6" lg="6" xl="6">
+          <div>
+            <v-img
+              contain
+              max-height="350"
+              :src="require('@/assets/img/illustration2.png')"
+            ></v-img>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6" lg="6" xl="6">
+          <div>
+            <h1 class="font-weight-medium">Tentang Kami</h1>
+            <p class="mt-2">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita istinctio.</p>
+          </div>
+        </v-col>        
+      </v-row>
+    </div>
+
+    <div class="py-12 mb-12">
+      <v-row justify="center" align="center">
+        <v-col cols="12" md="4" lg="4" xl="4" class="d-flex flex-column">
+          <h2 class="text-center font-weight-medium">Spesial untuk kamu</h2>
+          <p class="text-center">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere.</p>
+          <div class="d-flex flex-row justify-center align-center">
+            <v-icon color="success" class="my-auto">mdi-whatsapp</v-icon>
+            <p class="my-auto ml-2 font-weight-bold">0821-2312-3412</p>
+          </div>
+          <div class="mx-auto">
+            <v-btn color="primary" small class="mt-3">Hubungi sekarang</v-btn>
+          </div>
+        </v-col>        
+      </v-row>
     </div>
   </div>
 </template>
